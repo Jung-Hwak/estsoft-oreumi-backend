8ea612ddd5dff4fb0427fb31b23d33c5a9f905e2
Jung, Hwak <junghwak56@gmail.com>
2025-12-04 17:47:54 +0900

    객체의 식별자 이름 변경

--- 14-java/src/day05/Ex08.java
+++ 14-java/src/day05/Ex08.java
@@ -2,11 +2,11 @@ package day05;
 
 public class Ex08 {
     public static void main(String[] args) {
-        Student cheolsuKim = new Student("Cheolsu Kim", 88, 76,64);
-        Student YeongheePark = new Student("Yeonghee Park", 92, 88,94);
-        Student GildongHong = new Student("Gildong Hong", 96, 64,48);
-        cheolsuKim.print();
-        YeongheePark.print();
-        GildongHong.print();
+        Student kim = new Student("Cheolsu Kim", 88, 76,64);
+        Student park = new Student("Yeonghee Park", 92, 88,94);
+        Student hong = new Student("Gildong Hong", 96, 64,48);
+        kim.print();
+        park.print();
+        hong.print();
     }
 }
