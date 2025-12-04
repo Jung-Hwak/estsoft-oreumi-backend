35efa28f4920d9f8749ed41f48bee4023d9702ff
Jung, Hwak <junghwak56@gmail.com>
2025-12-04 22:50:36 +0900

    Java day04 demo 연습 문제
    풀이

--- 14-java/src/JavaDemo/Demo1203A.java
+++ 14-java/src/JavaDemo/Demo1203A.java
@@ -2,6 +2,24 @@ package JavaDemo;
 
 public class Demo1203A {
     public static void main(String[] args) {
+        final int[][] scores = {
+                { 87, 43, 64, 71 },
+                { 65, 69, 77, 82 },
+                { 91, 84, 62, 76 }
+        };
+        int[] sum = new int[scores[0].length];
+        double average;
+        final String[] ordinals = { "1st", "2nd", "3rd", "4th" };
 
+        for (int i = 0; i < scores.length; i++)
+            for (int j = 0; j < scores[i].length; j++)
+                sum[j] += scores[i][j];
+
+        for (int i = 0; i < scores[0].length; i++) {
+            average = (double)sum[i] / scores.length;
+
+            System.out.printf("%s column: sum = %d, average = %.2f\n",
+                    ordinals[i], sum[i], average);
+        }
     }
 }
--- 14-java/src/JavaDemo/Demo1203B.java
+++ 14-java/src/JavaDemo/Demo1203B.java
@@ -4,17 +4,32 @@ import java.util.Scanner;
 
 public class Demo1203B {
     public static void main(String[] args) {
+        final int LENGTH = 20;
         int num;
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        num = in.nextInt();
+        int decimal;
+        int digits;
+        int[] binary = new int[LENGTH];
 
+        Scanner in = new Scanner(System.in);
         while (true) {
-            if (num > 0) {
-                String binaryString = Integer.toBinaryString(num);
-                System.out.printf("The binary number for decimal %d is %s ", num, binaryString);
-            } else if (num == 0) break;
+            System.out.print("Enter a positive integer: ");
+            num = in.nextInt();
+            if (num == 0) break;
+            if (num < 1 || num > 1048575) {
+                System.out.println("ERROR: the entered value is out of range.\n");
+                continue;
+            }
+            decimal = num;
+            for (digits = 0; decimal != 0; digits++) {
+                binary[digits] = decimal % 2;
+                decimal /= 2;
+            }
+            System.out.printf("Binary number for decimal %d = ", num);
+            for (int i = digits - 1; i >= 0; i--)
+                System.out.print(binary[i]);
+            System.out.println("\n");
         }
+        System.out.println("Good bye!");
         in.close();
     }
 }
