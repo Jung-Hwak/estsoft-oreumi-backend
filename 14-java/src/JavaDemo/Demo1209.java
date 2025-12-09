8dd1eebdeffc5a1407560ce886768e0dcb3b17c5
Jung, Hwak <junghwak56@gmail.com>
2025-12-09 17:27:52 +0900

    Java deam1209 class 메서드 변경
    생성자 접근자 등을 바꾸고 print 출력을 바꿈

--- 14-java/src/JavaDemo/Demo1209.java
+++ 14-java/src/JavaDemo/Demo1209.java
@@ -1,7 +1,6 @@
 package JavaDemo;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 
 public class Demo1209 {
@@ -9,21 +8,21 @@ public class Demo1209 {
         Student1209 s1 = new Student1209(20255108, "James");
         Student1209 s2 = new Student1209(20255114, "Michael");
         Student1209 s3 = new Student1209(20255103, "Hans");
-        
+
         ArrayList<Student1209> list = new ArrayList<>();
         list.add(s1);
         list.add(s2);
         list.add(s3);
 
-        Collections.sort(list, new Comparator<Student1209>() {
+        list.sort(new Comparator<Student1209>() {
             @Override
             public int compare(Student1209 a, Student1209 b) {
-                return a.name.compareTo(b.name);
+                return a.getName().compareTo(b.getName());
             }
         });
 
         for (Student1209 stu : list) {
-            System.out.println("ID: " + stu.id + " Name: " + stu.name);
+            System.out.println(stu);
         }
     }
 }
--- 14-java/src/JavaDemo/Student1209.java
+++ 14-java/src/JavaDemo/Student1209.java
@@ -1,11 +1,24 @@
 package JavaDemo;
 
 public class Student1209 {
-    int id;
-    String name;
+    private int id;
+    private String name;
 
     public Student1209(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setId(int id) { this.id = id; }
+
+    public int getId() { return id; }
+
+    public String getName() { return  name; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " Name: " + name;
     }
 }
