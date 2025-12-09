1c60f59d389556877459c84afb8983eb4e933f0f
Jung, Hwak <junghwak56@gmail.com>
2025-12-09 17:19:16 +0900

    Java demo1209 풀이
    Java demo1209 연습 문제 풀이

---
+++ 14-java/src/JavaDemo/Student1209.java
@@ -0,0 +1,11 @@
+package JavaDemo;
+
+public class Student1209 {
+    int id;
+    String name;
+
+    public Student1209(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
