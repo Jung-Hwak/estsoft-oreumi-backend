6b0ffa0c95cade2ec5441f8aca5185e29cd30043
Jung, Hwak <junghwak56@gmail.com>
2026-01-13 20:11:07 +0900

    바인딩 포워드와 쿠키, 세션

--- 32-java-backend/step03/src/main/java/com/example/step03/ex02/SecondServlet.java
+++ 32-java-backend/step03/src/main/java/com/example/step03/ex02/SecondServlet.java
@@ -1,4 +1,4 @@
-package com.example.step03.ex01;
+package com.example.step03.ex02;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,12 +7,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/second")
+@WebServlet("/second2")
 public class SecondServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-        System.out.println("CALL: SecondServlet#doGet()");
+        System.out.println("CALL: SecondServlet2#doGet()");
+
+        // first2 서블릿에서 바인딩한 데이터를 참조
+        String name = (String)request.getAttribute("name");
 
         // 응답할 데이터의 MIME 타입 설정
         response.setContentType("text/html; charset=UTF-8");
@@ -21,7 +24,7 @@ public class SecondServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         // 클라이언트에 응답할 데이터를 HTML 형식으로 생성
-        String output = """
+        String output = String.format("""
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -32,9 +35,13 @@ public class SecondServlet extends HttpServlet {
 
 <h1>Dispatch Forward Test</h1>
 
+<ul>
+  <li>Name: %s</li>
+</ul>
+
 </body>
 </html>
-""";
+""", name);
 
         // 클라이언트에 응답할 데이터를 출력 스트림에 출력
         out.print(output);
