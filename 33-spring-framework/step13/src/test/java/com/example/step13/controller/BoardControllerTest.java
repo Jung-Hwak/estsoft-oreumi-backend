d8e34631e07651f48961bb79055b8a82ef6532e2
Jung, Hwak <junghwak56@gmail.com>
2026-01-24 14:56:13 +0900

    CRUD., Thymeleaf

--- 33-spring-framework/step13/src/test/java/com/example/step13/controller/BoardControllerTest.java
+++ 33-spring-framework/step13/src/test/java/com/example/step13/controller/BoardControllerTest.java
@@ -29,4 +29,55 @@ class BoardControllerTest {
                 .andExpect(status().isOk())
                 .andDo(print());
     }
+
+    @Test
+    public void testWrite() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/board/write")
+                .param("title", "[TEST] BoardControllerTest#testWrite")
+                .param("content", "[TEST] BoardControllerTest#testWrite")
+                .param("writer", "JUnit");
+
+        mockMvc.perform(request)
+                .andExpect(status().is3xxRedirection())
+                .andDo(print());
+    }
+
+    @Test
+    public void testRead() throws Exception {
+        final String id = "99";
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/board/read")
+                .param("id", id);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        final String id = "96";
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/board/update")
+                .param("id", id)
+                .param("title", "[TEST] BoardControllerTest#testUpdate")
+                .param("content", "[TEST] BoardControllerTest#testUpdate")
+                .param("writer", "JUnit");
+
+        mockMvc.perform(request)
+                .andExpect(status().is3xxRedirection())
+                .andDo(print());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        final String id = "91";
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/board/delete")
+                .param("id", id);
+
+        mockMvc.perform(request)
+                .andExpect(status().is3xxRedirection())
+                .andDo(print());
+    }
 }
